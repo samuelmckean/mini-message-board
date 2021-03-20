05671724799cbb4c65a8477d403b0f83c6cade50
Samuel McKean <samuel.mckean@gmail.com>
2021-03-20 18:43:48 -0500

    changed added display to approximate time ago the message was posted

--- frontend/src/Message.js
+++ frontend/src/Message.js
@@ -1,12 +1,15 @@
+import { formatDistanceToNow } from 'date-fns';
+
 function Message({ message }) {
   const { text, user, added } = message;
+  console.log(typeof added);
   return (
     <li>
       <div className='card'>
         <div className='card-body'>
           <h3 className='card-name'>{user}</h3>
           <p className='card-text'>{text}</p>
-          <div className='card-footer text-muted text-right'>{added}</div>
+          <div className='card-footer text-muted text-right'>{formatDistanceToNow(added) + ' ago'}</div>
         </div>
       </div>
     </li>
