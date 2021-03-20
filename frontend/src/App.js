a3f2a6aa74e8ce1825d2131a2f215c73fad251af
Samuel McKean <samuel.mckean@gmail.com>
2021-03-20 16:01:39 -0500

    added MessageBoard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import './App.css';
+import MessageBoard from './MessageBoard';
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Message Board</h1>
       </header>
+      <MessageBoard />
     </div>
   );
 }
