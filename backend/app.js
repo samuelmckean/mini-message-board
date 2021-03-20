b497ebcf7dfea8e1b1b3d09a5a35acd1a4441091
Samuel McKean <samuel.mckean@gmail.com>
2021-03-20 15:49:32 -0500

    added cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // message list to be removed and persisted with a database
