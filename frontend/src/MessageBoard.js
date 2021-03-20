54d478e1e473bc8219f3c80cf3ce952722dfdad1
Samuel McKean <samuel.mckean@gmail.com>
2021-03-20 17:43:12 -0500

    getMessages working

---
+++ frontend/src/MessageBoard.js
@@ -0,0 +1,50 @@
+import Message from './Message';
+import NewMessage from './NewMessage';
+import uniqid from 'uniqid';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+// set the base url for making API requests
+const instance = axios.create({
+  baseURL: 'http://localhost:3000',
+});
+
+// get the messages from the backend
+const getMessages = async () => {
+  try {
+    const response = await instance.get('/');
+    const messages = response.data;
+    return messages;
+  } catch {
+    return Error('Error getting messages');
+  }
+}
+
+// react component that contains messages and the new message form
+function MessageBoard() {
+  // create state for messages array
+  const [messages, setMessages] = useState([]);
+
+  // sets the state to the message from the backend on mount
+  useEffect(() => {
+    const fetchData = async () => {
+      const messages = await getMessages();
+      setMessages(messages);
+    }
+    fetchData();
+  }, []);
+
+  // create a Message component for each message
+  const messageItems = messages.map((message) => {
+    return <Message key={uniqid()} message={message} />
+  });
+
+  return (
+    <div>
+      <ul>{messageItems}</ul>
+      <NewMessage />
+    </div>
+  );
+}
+
+export default MessageBoard;
